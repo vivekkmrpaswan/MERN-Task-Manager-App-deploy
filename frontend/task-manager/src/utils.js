4b1a6ca99dcb0be1840613a554a2dc440dff31e5
vivek kumar <167101531+vivekkmrpaswan@users.noreply.github.com>
2025-05-08 11:56:57 +0530

    Deployed backend in render

--- frontend/task-manager/src/utils.js
+++ frontend/task-manager/src/utils.js
@@ -4,4 +4,7 @@ export const notify = (message, type)=>{
     toast[type](message)
 }
 
-export const API_URL = 'http://localhost:8080'
+// export const API_URL = 'http://localhost:8080'
+
+
+export const API_URL = 'https://mern-task-manager-app-deploy-1.onrender.com'
